a908436cfda1a10e6aeb8a5d6570188432959f41
ramonpineda <ramonpineda@Admins-Air.lan>
2015-05-21 16:24:28 -0500

    First commit

--- TestRestKit/MasterViewController.m
+++ TestRestKit/MasterViewController.m
@@ -6,22 +6,21 @@
 //  Copyright (c) 2015 ramonpineda. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MasterViewController.h"
+#import <RestKit.h>
 
-@interface ViewController ()
+@interface MasterViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MasterViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
